3384d1143212f06bb90aa33147a5483eabda97d4
Jeyhun Tahirov <jeyhunt@gmail.com>
2018-06-05 16:48:40 -0700

    tell a joke command

--- static/js/command_parser.js
+++ static/js/command_parser.js
@@ -1,3 +1,20 @@
+var tellJoke = function()
+{
+	var request = new XMLHttpRequest();
+
+	request.open('GET', 'http://api.icndb.com/jokes/random/', true);
+	request.onload = function () {
+		var data = JSON.parse(this.response);
+		if (request.status >= 200 && request.status < 400)
+		{
+			console.log(data.value.joke);
+			var msg = new SpeechSynthesisUtterance(data.value.joke);
+			window.speechSynthesis.speak(msg);
+		}
+	}
+	request.send();
+}
+
 var parseCommand = function(command)
 {
 	if (command.indexOf("SET AN ALARM") != -1)
@@ -10,5 +27,11 @@ var parseCommand = function(command)
 		interConnection.send("kws");
 		window.location.href = "http://google.com";
 	}
+	else if (command.indexOf("TELL A JOKE") != -1)
+	{
+		tellJoke();
+		interConnection.send("kws");
+	}
+	else
 		console.log(command);
 }
